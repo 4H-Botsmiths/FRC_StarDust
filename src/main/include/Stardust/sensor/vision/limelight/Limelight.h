7ea918e05b39b57d6460f52d84746cb5b50fbc6a
FRC-4309-DEV <FRC.4309.DEV@gmail.com>
2019-05-30 22:42:54 -0700

    moved limelight into seperate folder, added more funcs

--- src/main/include/Stardust/sensor/vision/limelight/Limelight.h
+++ src/main/include/Stardust/sensor/vision/limelight/Limelight.h
@@ -1,5 +1,8 @@
+#pragma once
+
 #include <networktables/NetworkTableInstance.h>
 #include <networktables/NetworkTable.h>
+#include <string.h>
 
 class Limelight {
 public:
@@ -7,6 +10,18 @@ public:
 
     //true turns lights on, false turns off
     void setLights(bool on) { mLimelight->PutNumber("ledMode", (on?3:1)); }
+
+    double get(std::string key) { return mLimelight->GetNumber(key, 0); }
+    double getTX() { return get("tx"); }
+    double getTY() { return get("ty"); }
+    double getTA() { return get("ta"); }
+    double getTS() {
+        double tmp=get("ts");
+
+        //make skew usable
+        if (tmp<-45) tmp+=90;
+        return tmp*-1;
+    }
 
 private:
     std::shared_ptr<NetworkTable> mLimelight=nt::NetworkTableInstance::GetDefault().GetTable("limelight");
