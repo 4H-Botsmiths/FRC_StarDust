c05a5607d287a31b1aa8974feea80f8831642b5d
FRC-4309-DEV <FRC.4309.DEV@gmail.com>
2019-08-15 13:50:22 -0700

    turned IR into a component

--- Stardust/sensor/vision/ir/IR.hpp
+++ Stardust/sensor/vision/ir/IR.hpp
@@ -1,11 +1,22 @@
 #include <frc/AnalogInput.h>
 
+#include "Stardust/core/StarDustComponent.hpp"
+
 //wrapper class for using an infa-red sensor
-class IR {
+class IR : public StarDustComponent {
 public:
     IR(int port) {
         sensor=new frc::AnalogInput(port);
     }
+
+    //in the future, these functions will cache all the data from the sensor each frame
+    void __RobotInit__() {};
+    void __RobotPeriodic__() {};
+    void __AutonomousInit__() {};
+    void __AutonomousPeriodic__() {};
+    void __TeleopInit__() {};
+    void __TeleopPeriodic__() {};
+    void __TestPeriodic__() {};
 
     double getV() { return sensor->GetVoltage(); }
     double getVV() { return sensor->GetValue(); }
