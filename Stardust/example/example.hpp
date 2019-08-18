fe6af73c271f68df2dab570782864e75cfeba72a
dosisod <39638017+dosisod@users.noreply.github.com>
2019-08-18 05:09:43 +0000

    added example component creation/usage

---
+++ Stardust/example/example.hpp
@@ -0,0 +1,23 @@
+#include "Stardust/core/StarDustComponent.hpp"
+
+class Example : public StarDustComponent {
+public:
+	//default constructor, here you could setup some parameters and such
+	Example() {}
+
+	//these functions will be automatically ran when StarDustRobot calls "RobotInit", "TeleopPeriodic", etc.
+    void __RobotInit__() override;
+    void __RobotPeriodic__() override;
+    void __AutonomousInit__() override;
+    void __AutonomousPeriodic__() override;
+    void __TeleopInit__() override;
+    void __TeleopPeriodic__() override;
+    void __TestPeriodic__() override;
+
+	//allows user to easily update the class manually if needed
+	void update();
+
+private:
+	//put the values you want to update here
+	int somevalue=-1;
+}
