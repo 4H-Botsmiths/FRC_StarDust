af2690eeef79afe67e269265e361d2ae8b5ca826
FRC-4309-DEV <FRC.4309.DEV@gmail.com>
2019-08-22 14:47:18 -0700

    added builtin dampening option for better motors

--- Stardust/motor/BetterMotor.hpp
+++ Stardust/motor/BetterMotor.hpp
@@ -8,11 +8,20 @@
 class BetterMotor : public StarDustComponent, public frc::PWMSpeedController {
 public:
     //create PWM controler from only port
-    BetterMotor(int n) : frc::PWMSpeedController(n) {}
+    BetterMotor(int n) : frc::PWMSpeedController(n), dampen(1) {}
 
     //create PWM controller from port and polarity
-    BetterMotor(int n, bool invert) : frc::PWMSpeedController(n) {
+    BetterMotor(int n, bool invert) : frc::PWMSpeedController(n), dampen(1) {
         SetInverted(invert);
+    }
+
+    //create a motor controller given port, polarity, and input dampening
+    BetterMotor(int n, double damp, bool invert) : frc::PWMSpeedController(n), dampen(damp) {
+        SetInverted(invert);
+    }
+
+    void Set(double input) {
+        PWMSpeedController::Set(input*dampen);
     }
 
     //required to be implemented by the drivebase to be considered a stardust component
@@ -23,4 +32,8 @@ public:
     void __TeleopInit__() {}
     void __TeleopPeriodic__() {}
     void __TestPeriodic__() {}
+
+private:
+    //multiplier to dampen the input to the motor controller
+    double dampen=0;
 };
