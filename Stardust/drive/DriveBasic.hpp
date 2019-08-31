69b9212dc28399edf38815351fe2fb2be4f98a62
FRC-4309-DEV <FRC.4309.DEV@gmail.com>
2019-08-30 20:29:10 -0700

    changed comments, added tank drive mode

--- Stardust/drive/DriveBasic.hpp
+++ Stardust/drive/DriveBasic.hpp
@@ -9,12 +9,12 @@ class DriveBasic : public DriveBase {
 public:
     //create drivetrain with default multipliers
     DriveBasic(frc::SpeedController* m0, frc::SpeedController* m1) : DriveBase() {
-        //make a new mecanum drive reference
+        //make a new basic drivetrain
         differential=new frc::DifferentialDrive(*m0, *m1);
     }
     //create drivetrain with custom multipliers (basic drive has no X movement)
     DriveBasic(frc::SpeedController* m0, frc::SpeedController* m1, float y, float r) : DriveBase(1, y, r) {
-        //make a new mecanum drive reference
+        //make a new basic drivetrain
         differential=new frc::DifferentialDrive(*m0, *m1);
     }
 
@@ -56,6 +56,12 @@ public:
             drive(
                 x->GetYLeftDeadzone()*gety(), //Y on left stick controls Y direction
                 x->GetXLeftDeadzone()*getr() //X on left stick controls turning
+            );
+        }
+        else if (mode==2) {
+            differential->TankDrive(
+                x->GetYRightDeadzone()*gety(), //Y on right controlls right motor speed
+                x->GetYLeftDeadzone()*gety() //Y on left controlls left motor speed
             );
         }
     }
