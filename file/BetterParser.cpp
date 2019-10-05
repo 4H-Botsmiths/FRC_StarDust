150371175726eb9d1b6cf1d34681c3917ce1273b
FRC-4309-DEV <FRC.4309.DEV@gmail.com>
2019-10-05 00:03:44 -0700

    added optional semi-colon option for param files

--- file/BetterParser.cpp
+++ file/BetterParser.cpp
@@ -17,14 +17,16 @@ void BetterParser::autorun() {
 
     if (stream.is_open()) {
         while (std::getline(stream, line)) { //loop through each line in the file
-            //split each line on the first = sign
-            int pos=line.find("=");
+            int pos=line.find("="); //split each line on the first = sign
+            bool semi=line.find(";")==(line.length()-1); //true if a semi-colon is at the end of the line
 
-            if (pos>0&&pos<line.length()-1) { //must be at least one character on each side of the =
+            if (pos>0&&(pos<line.length()-1-semi)) {  //must be at least one character on each side of the =
                 for (auto* param : parameters) { //loop throught and check try to parse each parameter
                     if (param->convert(
                         line.substr(0, pos), //left hand side
-                        line.substr(pos+1, line.length()) //right hand side
+                        semi ?
+                            line.substr(pos+1, line.length()) : //right hand side (no semi-colon)
+                            line.substr(pos+1, line.length()-1) //right hand side (with semi-colon)
                     )) break; //if parameter name is valid, skip to next file in line
                 }
             }
