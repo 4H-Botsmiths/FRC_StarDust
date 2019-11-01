c315b767b97009a82f5741d82890eb81b3126efb
FRC-4309-DEV <FRC.4309.DEV@gmail.com>
2019-10-31 20:51:03 -0700

    made spider drive funcitonal

--- drive/DriveSpider.hpp
+++ drive/DriveSpider.hpp
@@ -1,39 +1,32 @@
 #pragma once
 
-#include <frc/drive/MecanumDrive.h>
 #include <frc/SpeedController.h>
+#include <frc/drive/MecanumDrive.h>
 
+#include "StarDust/pneumatics/BetterDoubleSolenoid.hpp"
+#include "StarDust/drive/DriveMecanum.hpp"
 #include "StarDust/util/BetterTimer.hpp"
-#include "DriveBase.hpp"
 
 //DriveSpider (spider drive) is an expiremental drivetrain which uses both mecanum and normal wheels
-class DriveSpider: public DriveMecanum, public DriveBasic {
-public:
-				//creates spider drive with just 4 speed controllers, and 1x multipliers on all axis
+class DriveSpider : public DriveMecanum {
+   public:
+    //creates spider drive with just 4 speed controllers, and 1x multipliers on all axis
     DriveSpider(frc::SpeedController* m0, frc::SpeedController* m1, frc::SpeedController* m2, frc::SpeedController* m3, BetterDoubleSolenoid* solenoid) :
-								DriveSpider(m0, m1, m2, m3, 1, 1, 1),
-								shifter(solenoid)
-								{}
+        DriveSpider(m0, m1, m2, m3, solenoid, 1, 1, 1) {}
 
-				//creates spider drive with the 4 speed controllers, but uses custom multipliers
+    //creates spider drive with the 4 speed controllers, but uses custom multipliers
     DriveSpider(frc::SpeedController* m0, frc::SpeedController* m1, frc::SpeedController* m2, frc::SpeedController* m3, BetterDoubleSolenoid* solenoid, float x, float y, float r) :
-								DriveMecanum( //creates mecanum drive train
-												*m0,
-												*m3,
-												*m1,
-												*m2,
-												x,
-												y,
-												r
-								),
-								DriveBasic( //creates normal drive train
-            new frc::SpeedControllerGroup(*m0, *m3),
-            new frc::SpeedControllerGroup(*m1, *m2),
-												y,
-												r
-								),
-								shifter(solenoid)
-								{}
+        DriveMecanum( //creates mecanum drive train
+            m0,
+            m1,
+            m2,
+            m3,
+            x,
+            y,
+            r
+        ),
+        shifter(solenoid)
+        {}
 
     //required to be implemented by the drivebase to be considered a stardust component
     void __RobotInit__() {}
@@ -44,39 +37,18 @@ public:
     void __TeleopPeriodic__() {}
     void __TestPeriodic__() {}
 
-    void drive(float y) {
-								if (usingMec) {
-												//mecanum->DriveCartesian(y*gety(), 0, 0);
-												DriveMecanum::drive(y);
-								}
-								else {
-												DriveBasic::drive(y);
-								}
-    }
-
-    void drive(float y, float r) {
-        if (usingMec) {
-												DriveMecanum::drive(y, r);
-												//mecanum->DriveCartesian(y*gety(), 0, r*getr());
-								}
-								else {
-												DriveBasic::drive(y, r);
-								}
-    }
-
     void drive(float x, float y, float r) {
-								if (usingMec) {
- 				       //mecanum->DriveCartesian(-x*getx(), y*gety(), r*getr());
-												DriveMecanum::drive(x, y ,r);
-								}
-								else {
-												DriveBasic::drive(0, y, r);
-								}
+        if (usingMec) {
+            DriveMecanum::drive(x, y, r);
+        }
+        else {
+            DriveMecanum::drive(0, y, r);
+        }
     }
 
     void drive(float x, float y, float r, float t) {
-        BetterTimer{true, [=]{
-												//calls drive from current class to make sure useMec is respected
+        BetterTimer{true, [=] {
+            //calls drive from current class to make sure useMec is respected
             DriveSpider::drive(x, y, r);
         }, t};
     }
@@ -89,29 +61,29 @@ public:
         );
     }
 
-				void useMecanum() { //go into mecanum mode
-								usingMec=true;
-								solenoid->Retract();
-				}
+    void useMecanum() {  //go into mecanum mode
+        usingMec = true;
+        shifter->Retract();
+    }
 
-				void useNormal() { //go into normal mode
-								usingMec=false;
-								solenoid->Extend();
-				}
+    void useNormal() {  //go into normal mode
+        usingMec = false;
+        shifter->Extend();
+    }
 
-				void invert() { //invert current mode state
-								if (usingMec) {
-												useNormal();
-								}
-								else {
-												useMec();
-								}
-				}
+    void invert() {  //invert current mode state
+        if (usingMec) {
+            useNormal();
+        }
+        else {
+            useMecanum();
+        }
+    }
 
 private:
-				//true if mecs are in use, false if normal wheels are in use
-				bool usingMec=true;
+    //true if mecs are in use, false if normal wheels are in use
+    bool usingMec=true;
 
-				//double solenoid to use when shifting between the 2 modes
-				BetterDoubleSolenoid* shifter;
+    //double solenoid to use when shifting between the 2 modes
+    BetterDoubleSolenoid* shifter;
 };
